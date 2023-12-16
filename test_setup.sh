0b402c879160355369477836e74d9de92a1f41f7
kvdmitrieva <katedv02@gmail.com>
2023-12-17 00:08:41 +0300

    final model

--- test_setup.sh
+++ test_setup.sh
@@ -1,3 +1,3 @@
 echo "Download checkpoint"
 gdown "https://drive.google.com/u/0/uc?id=1freCZqG2TRuUaBJ4ggl1hmdbO3CTcBwC" -O default_test_model/config.json
-gdown "https://drive.google.com/u/0/uc?id=1rJyD1TrNT77sEeqLuqRZsI4KxIQo_Mou" -O default_test_model/model.pth
+gdown "https://drive.google.com/u/0/uc?id=1M58t4FqqT8g4NBHz9jB-_lwuZ6KAlnl7" -O default_test_model/model.pth
